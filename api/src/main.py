5b4bb8f7c5df90d82bcf5c4bc7b62cc975c1f3a5
angelsornichero <angelspac08@gmail.com>
2023-05-22 22:30:41 +0200

    Another modification

--- api/src/main.py
+++ api/src/main.py
@@ -1,8 +1,14 @@
 from app import App
-from constants.main import PORT
+from constants.main import PORT, ACTION_ENV
+import sys
+import time 
+
 
 app = App().app
 
 if __name__ == "__main__":
    
    app.run(port=PORT, debug=True)
+   if ACTION_ENV == 1:
+      time.sleep(3)
+      sys.exit(0)
